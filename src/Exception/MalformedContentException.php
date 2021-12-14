43f26e6e24194f58aa07ed3cc6251688f40a3079
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2021-12-14 21:59:13 +0100

    added MalformedContentException

---
+++ src/Exception/MalformedContentException.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace SBSEDV\Component\InputConverter\Exception;
+
+class MalformedContentException extends \Exception implements ExceptionInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(?\Throwable $previous = null)
+    {
+        parent::__construct('The request content structure does not match the expected format.', previous: $previous);
+    }
+}
