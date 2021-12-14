e6743d2b204bb255e17743b62604551066c69186
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2021-12-14 21:58:43 +0100

    added ExceptionInterface

---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace SBSEDV\Component\InputConverter\Exception;
+
+/**
+ * Base exception interface for all input converter thrown exceptions.
+ */
+interface ExceptionInterface extends \Throwable
+{
+}
