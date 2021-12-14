0437de0adddc4419033ebb3a5ae9c389be71c6d1
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2021-12-14 21:16:05 +0100

    added AbstractConverter implementation

---
+++ src/Converter/AbstractConverter.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace SBSEDV\Component\InputConverter\Converter;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+abstract class AbstractConverter implements ConverterInterface
+{
+    /**
+     * Get the content type header of the given request.
+     *
+     * @param Request|ServerRequestInterface $request The request to get the header from.
+     */
+    protected function getContentType(Request | ServerRequestInterface $request): string
+    {
+        $headers = [];
+
+        if ($request instanceof Request) {
+            $headers = $request->headers->all('Content-Type');
+        } else {
+            $headers = $request->getHeader('Content-Type');
+        }
+
+        $contentType = reset($headers);
+
+        return \is_string($contentType) ? $contentType : '';
+    }
+
+    /**
+     * Get the content (body) of the request as string.
+     *
+     * @param Request|ServerRequestInterface $request The request to get the content from.
+     */
+    protected function getContent(Request | ServerRequestInterface $request): string
+    {
+        if ($request instanceof Request) {
+            return $request->getContent();
+        }
+
+        return (string) $request->getBody();
+    }
+}
